060329b370743a5b7d05b271f50820d48c0d3905
CameronsmithP <cameronsmithp@gmail.com>
2022-04-11 13:44:39 -0600

    got the image to pull up on image anchor
    now we need to get the object to stay without the use of an image and make it go away when you walk away from the portal

--- preViews/ViewController.swift
+++ preViews/ViewController.swift
@@ -5,80 +5,50 @@
 //  Created by Cameron Smith on 4/1/22.
 //
 
-import UIKit
+import SwiftUI
 import RealityKit
-import SceneKit
 import ARKit
 
 
-class ViewController: UIViewController, ARSessionDelegate {
+struct ARPortalView: UIViewRepresentable {
     
-    @IBOutlet var arView: ARView!
     
    // let boxAnchor = try! SpaceUmbrellas.Umbrellas()
     //var imageAnchorToEnitity: [ARImageAnchor: AnchorEntity] = [:]
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        //arView.delegate = self
-       // arView.scene.addAnchor(boxAnchor)
-       // arView.session.delegate = self
-        // Load the "Box" scene from the "Experience" Reality File
-        // Show statistics such as fps and timing information
+   
+    
+    func makeUIView(context: Context) -> ARView {
         let innerSpaceSize: Float = 3.0 // [meters]
-          let innerSpaceMargin: Float = 0.02 // [meters]
-          let innerSpaceOcclusion: Float = 0.01 // [meters]
-          
-          // Add the box anchor to the scene
-          let portalAnchor = try! Experience.loadScene()
-          arView.scene.anchors.append(portalAnchor)
-          let boxSize: Float = innerSpaceSize + innerSpaceMargin
-          let boxMesh = MeshResource.generateBox(size: boxSize)
-          let material = OcclusionMaterial()
-          let occlusionBox = ModelEntity(mesh: boxMesh, materials: [material])
-          occlusionBox.position.y = boxSize / 2
-          occlusionBox.position.z = -(innerSpaceMargin + innerSpaceOcclusion)
-          
-          // make door using occlusion
-          portalAnchor.addChild(occlusionBox)
-        
-        
-        
-        
+        let innerSpaceMargin: Float = 0.02 // [meters]
+        let innerSpaceOcclusion: Float = 0.01 // [meters] for to create a door inside the inner space
+
+        let arView = ARView(frame: .zero)
+
+        // load one of bundled rcprojects as an inner space
+        let portalAnchor = try! SpaceUmbrellas.loadUmbrellas()   // Portal 1: Umbrellas
+        // let portalAnchor = try! SpaceXmas.loadScene()        // Portal 2: Xmas
+        // let portalAnchor = try! SpaceNight.loadScene()      // Portal 3: Night
+
+        arView.scene.anchors.append(portalAnchor)
+
+        // make an occlusion box that surrounds the inner space.
+        let boxSize: Float = innerSpaceSize + innerSpaceMargin
+        let boxMesh = MeshResource.generateBox(size: boxSize)
+        let material = OcclusionMaterial()
+        let occlusionBox = ModelEntity(mesh: boxMesh, materials: [material])
+        occlusionBox.position.y = boxSize / 2
+        occlusionBox.position.z = -(innerSpaceMargin + innerSpaceOcclusion) // make door using occlusion
+        portalAnchor.addChild(occlusionBox)
+
+        // start the AR session.
+       // let config = ARWorldTrackingConfiguration()
+       // config.planeDetection = [.horizontal]
+        //arView.session.run(config)
+
+        return arView
     }
-   /* func session(_session:ARSession, didAdd anchors: [ARAnchor]){
-        anchors.compactMap {$0 as? ARImageAnchor}.forEach{
-            let anchorEntity = AnchorEntity()
-            let modelEntity = boxAnchor.!
-            anchorEntity.addChild(modelEntity)
-            arView.scene.addAnchor(anchorEntity)
-            anchorEntity.transform.matrix = $0.transform
-            imageAnchorToEnitity[$0] = anchorEntity
-        }
-    }*/
-   
-                        
-                      /*
-                       */
-       
-    
-   
-        
-        
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+
+    func updateUIView(_ uiView: ARView, context: Context) {
     }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
-    
 }
