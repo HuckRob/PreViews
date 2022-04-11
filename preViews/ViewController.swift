6e9c8c79391edfadfe4fbb90ef0989791a047236
CameronsmithP <cameronsmithp@gmail.com>
2022-04-11 12:56:48 -0600

    easy simple solution fix

--- preViews/ViewController.swift
+++ preViews/ViewController.swift
@@ -11,82 +11,58 @@ import SceneKit
 import ARKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSessionDelegate {
     
     @IBOutlet var arView: ARView!
+    
+   // let boxAnchor = try! SpaceUmbrellas.Umbrellas()
+    //var imageAnchorToEnitity: [ARImageAnchor: AnchorEntity] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //arView.delegate = self
-        
+       // arView.scene.addAnchor(boxAnchor)
+       // arView.session.delegate = self
         // Load the "Box" scene from the "Experience" Reality File
         // Show statistics such as fps and timing information
+        let innerSpaceSize: Float = 3.0 // [meters]
+          let innerSpaceMargin: Float = 0.02 // [meters]
+          let innerSpaceOcclusion: Float = 0.01 // [meters]
+          
+          // Add the box anchor to the scene
+          let portalAnchor = try! Experience.loadScene()
+          arView.scene.anchors.append(portalAnchor)
+          let boxSize: Float = innerSpaceSize + innerSpaceMargin
+          let boxMesh = MeshResource.generateBox(size: boxSize)
+          let material = OcclusionMaterial()
+          let occlusionBox = ModelEntity(mesh: boxMesh, materials: [material])
+          occlusionBox.position.y = boxSize / 2
+          occlusionBox.position.z = -(innerSpaceMargin + innerSpaceOcclusion)
+          
+          // make door using occlusion
+          portalAnchor.addChild(occlusionBox)
+        
+        
+        
+        
+    }
+   /* func session(_session:ARSession, didAdd anchors: [ARAnchor]){
+        anchors.compactMap {$0 as? ARImageAnchor}.forEach{
+            let anchorEntity = AnchorEntity()
+            let modelEntity = boxAnchor.!
+            anchorEntity.addChild(modelEntity)
+            arView.scene.addAnchor(anchorEntity)
+            anchorEntity.transform.matrix = $0.transform
+            imageAnchorToEnitity[$0] = anchorEntity
+        }
+    }*/
+   
+                        
+                      /*
+                       */
+       
     
-        
-        
-        
-        
-    }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-        if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed:"AR Resources",bundle:Bundle.main){
-            configuration.detectionImages = imageToTrack
-            configuration.maximumNumberOfTrackedImages = 2
-        }
-        // Run the view's session
-        arView.session.run(configuration)
-    }
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        arView.session.pause()
-    }
-    
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-        
-        if let imageAnchor = anchor as? ARImageAnchor{
-            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width,height:imageAnchor.referenceImage.physicalSize.height)
-            let planeNode = SCNNode(geometry:plane)
-            planeNode.eulerAngles.x = -.pi/2
-            node.addChildNode(planeNode)
-            
-            if imageAnchor.referenceImage.name ==  "Chipotle" {
-                if let modelScene = SCNScene(named:"Experience"){
-                    if let modelNode = modelScene.rootNode.childNodes.first{
-                        planeNode.addChildNode(modelNode)
-                        modelNode.eulerAngles.x = .pi/2
-                        
-                       let innerSpaceSize: Float = 3.0 // [meters]
-                        let innerSpaceMargin: Float = 0.02 // [meters]
-                        let innerSpaceOcclusion: Float = 0.01 // [meters]
-                        
-                        // Add the box anchor to the scene
-                        let portalAnchor = try! Experience.loadScene()
-                        arView.scene.anchors.append(portalAnchor)
-                        let boxSize: Float = innerSpaceSize + innerSpaceMargin
-                        let boxMesh = MeshResource.generateBox(size: boxSize)
-                        let material = OcclusionMaterial()
-                        let occlusionBox = ModelEntity(mesh: boxMesh, materials: [material])
-                        occlusionBox.position.y = boxSize / 2
-                        occlusionBox.position.z = -(innerSpaceMargin + innerSpaceOcclusion)
-                        
-                        // make door using occlusion
-                        portalAnchor.addChild(occlusionBox)
-                       
-        }
-                }
-            }
-            
-                    
-        }
-        return node
-    }
    
         
         
