74f86c44c8d6dbec5d05843b1359d72d1c821c72
CameronsmithP <cameronsmithp@gmail.com>
2022-04-15 15:13:30 -0600

    previews version 2

--- preViews/ViewController.swift
+++ preViews/ViewController.swift
@@ -43,18 +43,19 @@ struct ARPortalView: UIViewRepresentable {
 
         // start the AR session.
       //declares reference images as the assete AR Resources
-       /* guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
+        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
             fatalError("Missing expected asset catalog resources.")
-        }*/
+        }
+        //arView.session.run(config)
+        let config = ARWorldTrackingConfiguration()
+        config.detectionImages = referenceImages// your images here
+       config.maximumNumberOfTrackedImages = 1 // or up to 4
+        arView.session.run(config, options: [.resetTracking, .removeExistingAnchors])
+
+        //also set other options like plane detection and environment texture if you want
        // arView.session.run(config)
        // let config = ARWorldTrackingConfiguration()
-        //config.detectionImages = referenceImages// your images here
-       // config.maximumNumberOfTrackedImages = 1 // or up to 4
-        // also set other options like plane detection and environment texture if you want
-        //arView.session.run(config)
-       /* let config = ARWorldTrackingConfiguration()
-        config.detectionImages = referenceImages
-        arView.session.run(config, options: [.resetTracking, .removeExistingAnchors])*/
+       // config.detectionImages = referenceImages
 
         return arView
     }
